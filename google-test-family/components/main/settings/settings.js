fe596b21456a73ddc751b78b64eaa90d06c232c3
Remek-P <remek.piotrowski@wp.pl>
2024-01-03 15:44:44 +0100

    search styling nearly done settings started sign-in started

--- google-test-family/components/main/settings/settings.js
+++ google-test-family/components/main/settings/settings.js
@@ -1,11 +1,15 @@
+import classes from "./settings.module.scss";
+
 function Settings() {
   return (
-      <div >
+      <div className={classes.settings}>
         <svg xmlns="http://www.w3.org/2000/svg"
-             height="24"
              viewBox="0 0 24 24"
+             height="24"
              width="24"
-             focusable="false">
+             focusable="false"
+             className={classes.settingsSVG}
+        >
           <path
               d="M12 16.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5zM10.5 12c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5zm0-6c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5z">
           </path>
--- google-test-family/components/navigation/navigation.js
+++ google-test-family/components/navigation/navigation.js
@@ -2,18 +2,18 @@ import classes from "./navigation.module.scss";
 import Hamburger from "@/components/navigation/hamburger/hamburger";
 import Logo from "@/components/navigation/logo/logo";
 import Search from "@/components/navigation/search/search";
-import Settings from "@/components/main/settings";
+import Settings from "@/components/main/settings/settings";
 import SignIn from "@/components/main/sign-in/sign-in";
 
 function Navigation() {
   return (
       <div className={classes.navigationContainer}>
-        <div className={classes.wrapperLeft}>
+        <div className={classes.wrapper}>
           <Hamburger />
           <Logo />
         </div>
           <Search />
-        <div  className={classes.wrapperRight}>
+        <div className={classes.wrapper}>
           <Settings />
           <SignIn />
         </div>
