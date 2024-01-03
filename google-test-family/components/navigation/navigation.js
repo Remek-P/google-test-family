b79112d10dcfae72beccae47a65688f87b6210ed
Remek-P <remek.piotrowski@wp.pl>
2024-01-03 15:26:15 +0100

    search styling nearly done settings started sign-in started

--- google-test-family/components/navigation/navigation.js
+++ google-test-family/components/navigation/navigation.js
@@ -2,6 +2,8 @@ import classes from "./navigation.module.scss";
 import Hamburger from "@/components/navigation/hamburger/hamburger";
 import Logo from "@/components/navigation/logo/logo";
 import Search from "@/components/navigation/search/search";
+import Settings from "@/components/main/settings";
+import SignIn from "@/components/main/sign-in/sign-in";
 
 function Navigation() {
   return (
@@ -11,8 +13,9 @@ function Navigation() {
           <Logo />
         </div>
           <Search />
-        <div>
-          Tutaj miejsce na login
+        <div  className={classes.wrapperRight}>
+          <Settings />
+          <SignIn />
         </div>
       </div>
   );
