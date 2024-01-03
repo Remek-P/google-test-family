a86368bfc1cf065c3511ebc9e23254a8339dcabc
Remek-P <remek.piotrowski@wp.pl>
2024-01-03 14:59:06 +0100

    hamburger styling update (no functionality) logo styling update (no functionality) search styling nearly done settings started sign-in started

--- google-test-family/components/navigation/navigation.js
+++ google-test-family/components/navigation/navigation.js
@@ -1,12 +1,19 @@
 import classes from "./navigation.module.scss";
 import Hamburger from "@/components/navigation/hamburger/hamburger";
 import Logo from "@/components/navigation/logo/logo";
+import Search from "@/components/navigation/search/search";
 
 function Navigation() {
   return (
       <div className={classes.navigationContainer}>
-        <Hamburger />
-        <Logo />
+        <div className={classes.wrapperLeft}>
+          <Hamburger />
+          <Logo />
+        </div>
+          <Search />
+        <div>
+          Tutaj miejsce na login
+        </div>
       </div>
   );
 }
