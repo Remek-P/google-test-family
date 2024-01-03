db8b44d13b2ac6b95050bb44440cdaa935591545
Remek-P <remek.piotrowski@wp.pl>
2024-01-03 11:27:56 +0100

    hamburger styling done (no functionality) logo styling done (no functionality)

--- google-test-family/components/navigation/hamburger/hamburger.js
+++ google-test-family/components/navigation/hamburger/hamburger.js
@@ -3,7 +3,9 @@ import classes from "./hamburger.module.scss";
 function Hamburger() {
   return (
       <div className={classes.hamburgerContainer}>
-        <svg className={classes.hamburgerSVG} xmlns="http://www.w3.org/2000/svg"
+        <svg
+             xmlns="http://www.w3.org/2000/svg"
+             className={classes.hamburgerSVG}
              viewBox="0 0 24 24"
              width="24"
              height="24"
--- google-test-family/components/navigation/navigation.js
+++ google-test-family/components/navigation/navigation.js
@@ -1,10 +1,12 @@
 import classes from "./navigation.module.scss";
 import Hamburger from "@/components/navigation/hamburger/hamburger";
+import Logo from "@/components/navigation/logo/logo";
 
 function Navigation() {
   return (
       <div className={classes.navigationContainer}>
         <Hamburger />
+        <Logo />
       </div>
   );
 }
