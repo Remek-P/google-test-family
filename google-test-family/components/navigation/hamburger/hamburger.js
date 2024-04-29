3e3165d79099fd5444de735151939ff9aa2920b0
Remek-P <remek.piotrowski@wp.pl>
2024-04-29 13:37:24 +0200

    refactoring

--- google-test-family/components/navigation/hamburger/hamburger.js
+++ google-test-family/components/navigation/hamburger/hamburger.js
@@ -2,14 +2,14 @@ import classes from "./hamburger.module.scss";
 
 function Hamburger() {
   return (
-      <button className={classes.hamburgerContainer}>
-        <svg
-             xmlns="http://www.w3.org/2000/svg"
+      <button className={classes.hamburgerContainer} tabIndex="-1">
+        <svg xmlns="http://www.w3.org/2000/svg"
              className={classes.hamburgerSVG}
              viewBox="0 0 24 24"
              width="24"
              height="24"
-             focusable="false">
+             focusable="false"
+             tabIndex="0">
           <path d="M21 6H3V5h18v1zm0 5H3v1h18v-1zm0 6H3v1h18v-1z"></path>
         </svg>
       </button>
