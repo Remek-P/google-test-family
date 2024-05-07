1252327e79eabf70e01c5251b34aa0e1785a4e59
Remek-P <remek.piotrowski@wp.pl>
2024-05-07 12:46:08 +0200

    MyLink - rel="noreferrer"

--- google-test-family/components/my-link/my-link.js
+++ google-test-family/components/my-link/my-link.js
@@ -3,7 +3,7 @@ import classes from "./my-link.module.scss";
 
 function MyLink({ where = "/", children, linkClass = "regular" }) {
   return (
-      <Link href={where} className={classes[linkClass]} rel="no-referrer">{ children }</Link>
+      <Link href={where} className={classes[linkClass]} rel="noreferrer">{ children }</Link>
   );
 }
 
