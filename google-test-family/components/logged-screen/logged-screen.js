999221b19792d98b154b74d84941145087648bf6
Remek-P <remek.piotrowski@wp.pl>
2024-05-07 15:16:34 +0200

    reformatting

--- google-test-family/components/logged-screen/logged-screen.js
+++ google-test-family/components/logged-screen/logged-screen.js
@@ -11,7 +11,7 @@ function LoggedScreen() {
           <p>Kliknięcie w szemrany link z nieznanego adresu email;</p>
           <p>Niesprawdzenie czy to na pewno YouTube - pasek adresu;</p>
           <p>Nierozpoznanie, że strona trochę się różni od prawdziwego YouTube;</p>
-          <p>Zalogowanie się;</p>
+          <p>Zalogowanie się na pałę;</p>
           <p> I ostatnie najważniejsze - niesłuchanie MNIE!</p>
         </div>
       </main>
