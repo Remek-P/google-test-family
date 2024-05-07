8ac558b2b257864aed9c9106e706115d8fb3751c
Remek-P <remek.piotrowski@wp.pl>
2024-05-07 12:38:22 +0200

    iframe for rick roll - proper MyLink - rel="no-referrer"

--- google-test-family/components/my-link/my-link.js
+++ google-test-family/components/my-link/my-link.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import classes from "./my-link.module.scss";
 
-function MyLink({ where = "/", children, linkClass = "regular", style = null }) {
+function MyLink({ where = "/", children, linkClass = "regular" }) {
   return (
-      <Link href={where} className={classes[linkClass]} style={{style}}>{ children }</Link>
+      <Link href={where} className={classes[linkClass]} rel="no-referrer">{ children }</Link>
   );
 }
 
--- google-test-family/components/rickRolled/rick-rolled.js
+++ google-test-family/components/rickRolled/rick-rolled.js
@@ -1,19 +1,26 @@
+import classes from "./rick-rolled.module.scss";
+
 function RickRolled() {
 
-  const address = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  const address = "https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?si=zot5uBVqB0_7cYqV?autoplay=1"
 
   return (
       <div>
         <h1>
           Nie dość, że to był szemrany link, który mógł robić dziwne rzeczy, to jeszcze haker dostałby dane logowania!
         </h1>
-        <iframe src={address}
-                title="Rick Rolled"
-                sandbox
-                loading="eager"
-                allow="autoplay">
+        <div className={classes.iframeContainer}>
+          <iframe width="100%"
+                  height="100%"
+                  src={address}
+                  title="YouTube video player"
+                  allow="autoplay"
+                  referrerPolicy="no-referrer"
+                  sandbox
+                  allowFullScreen>
 
-        </iframe>
+          </iframe>
+        </div>
       </div>
   );
 }
